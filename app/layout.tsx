704865888d293ea76f36899e905e2798ab72997d
seemiin <seemiin.wong@ecoworld.my>
2023-12-08 11:33:27 +0800

    change:apps layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,6 @@
 import '@/app/ui/global.css';
-import { Inter, Lusitana } from 'next/font/google';
+import { inter } from '@/app/ui/fonts';
 
-export const inter = Inter({ subsets: ['latin'] });
- 
-export const lusitana = Lusitana({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-});
 
 export default function RootLayout({
   children,
